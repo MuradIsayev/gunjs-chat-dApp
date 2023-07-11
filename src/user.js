a80cc55d13e40227710857815b677d29373f9380
MurAd <muradisayevazer@gmail.com>
2023-07-12 00:34:55 +0400

    bug fixed

--- src/user.js
+++ src/user.js
@@ -1,13 +1,13 @@
 import GUN from 'gun';
 import 'gun/sea'; // Security, Encryption, Authorization, enables user authentication
 import 'gun/axe'; // Advanced Exchange Equation, Connects to other peers, enables peer-to-peer communication
-import { Writable } from 'svelte/store'; // Svelte store, enables state management across components
+import { writable } from 'svelte/store'; // Svelte store, enables state management across components
 
 // Database
 export const db = GUN();
 
 // User
-export const user = db.user().recall({ sessionStorage: true });
+export const user = db.user().recall({ sessionStorage: true }); // User's data, stored in session storage
 
 // Current User's username
 export const username = writable('');
